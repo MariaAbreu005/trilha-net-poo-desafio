dff71035beb554b94dbe20ff686807d74ab8fc28
Maria Abreu <137839112+MariaAbreu005@users.noreply.github.com>
2023-11-22 07:03:02 -0300

    resolvendo o desafio de projeto POO

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,23 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herda da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor da classe Iphone
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        // Sobrescreve o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+        
+            Console.WriteLine($"Instalando {nomeApp} no iPhone...");
+           
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia  : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor da classe Nokia
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            public override void InstalarAplicativo(string nomeApp)
+            {
+            // instalação de aplicativo em um Nokia
+            Console.WriteLine($"Instalando {nomeApp} no Nokia...");
+            }
+        }
     }
 }
